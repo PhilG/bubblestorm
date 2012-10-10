7a9fbb1bf9d01d49137fe9f0ce69980848f2b459
PhilG <philg.github@gmail.com>
2012-10-10 12:04:47 +0200

    correctly indenting code using vim

--- js/application.js
+++ js/application.js
@@ -1,46 +1,44 @@
-$(document).ready(function() {    
-	var $input = $('#input');
+$(document).ready(function() {
+    var $input = $('#input');
     var $output = $('#output');
-    
-	
-    
+
     // Setting up CodeMirror
-    var myCodeMirror = CodeMirror($input.get(0), {      
+    var myCodeMirror = CodeMirror($input.get(0), {
         lineNumbers: true,
         matchBrackets: true,
-	    lineWrapping: true,
+        lineWrapping: true,
         mode:  "gfm",
         theme: "bubblestorm",
-        
-        onChange: function() {            
-    		$output.html(marked(myCodeMirror.getValue()));
-		    Rainbow.color();
-		    
-		    resizeCodeMirror();
+
+        onChange: function() {
+            $output.html(marked(myCodeMirror.getValue()));
+            Rainbow.color();
+
+            resizeCodeMirror();
         },
-        
+
         onScroll: function () {
-          var scrolled = $(".CodeMirror-scroll").scrollTop() / ($(".CodeMirror-scroll")[0].scrollHeight - $(".CodeMirror-scroll").height()) * 100;
-          
-          var toScroll = scrolled * ($("#output")[0].scrollHeight - $("#output").height()) / 100 ;
-          
-          $('#output').scrollTop(toScroll);     
-          
+            var scrolled = $(".CodeMirror-scroll").scrollTop() / ($(".CodeMirror-scroll")[0].scrollHeight - $(".CodeMirror-scroll").height()) * 100;
+
+            var toScroll = scrolled * ($("#output")[0].scrollHeight - $("#output").height()) / 100 ;
+
+            $('#output').scrollTop(toScroll);
+
         },
-      });
-      
-      function resizeCodeMirror() {
-		var pageHeight = $(window).height();
-		var footerHeight = $(".footer").outerHeight(true);
-		var headerHeight = $(".page-header").outerHeight(true);
-		// console.log(pageHeight, footerHeight);
-		myCodeMirror.setSize(null, pageHeight - footerHeight - headerHeight - 3);
-		myCodeMirror.refresh();
-	  }           
-        
-      $(window).resize(function() {
-			resizeCodeMirror();
-      });
-      
-  	  resizeCodeMirror(); 
     });
+      
+    function resizeCodeMirror() {
+        var pageHeight = $(window).height();
+        var footerHeight = $(".footer").outerHeight(true);
+        var headerHeight = $(".page-header").outerHeight(true);
+        // console.log(pageHeight, footerHeight);
+        myCodeMirror.setSize(null, pageHeight - footerHeight - headerHeight - 3);
+        myCodeMirror.refresh();
+    }
+        
+    $(window).resize(function() {
+        resizeCodeMirror();
+    });
+
+    resizeCodeMirror(); 
+});
--- js/boot.js
+++ js/boot.js
@@ -2,12 +2,12 @@
 LazyLoad.js("http://code.jquery.com/jquery-latest.js");
 
 LazyLoad.js("vendor/marked.js", function () {
-	// Setup Marked
-	marked.setOptions({
-		gfm: true,
-		pedantic: false,
-		sanitize: true,
-	});
+    // Setup Marked
+    marked.setOptions({
+        gfm: true,
+        pedantic: false,
+        sanitize: true,
+    });
 });
 
 // Load rainbow + languages
@@ -15,22 +15,22 @@ LazyLoad.js("vendor/rainbow/rainbow.js");
 
 var base = "vendor/rainbow/language/"
 var languages = [base + "c.js",
-				base + "coffeescript.js",
-				base + "css.js",
-				base + "generic.js",
-				base + "go.js",
-				base + "html.js",
-				base + "java.js",
-				base + "javascript.js",
-				base + "lua.js",
-				base + "php.js",
-				base + "python.js",
-				base + "r.js",
-				base + "ruby.js",
-				base + "scheme.js",
-				base + "shell.js",
-				base + "smalltalk.js"];
-				
+                base + "coffeescript.js",
+                base + "css.js",
+                base + "generic.js",
+                base + "go.js",
+                base + "html.js",
+                base + "java.js",
+                base + "javascript.js",
+                base + "lua.js",
+                base + "php.js",
+                base + "python.js",
+                base + "r.js",
+                base + "ruby.js",
+                base + "scheme.js",
+                base + "shell.js",
+                base + "smalltalk.js"];
+
 LazyLoad.js(languages);
 
 // Load codemirror + modes
@@ -39,24 +39,24 @@ LazyLoad.js("vendor/codemirror/lib/util/overlay.js");
 
 var base = "vendor/codemirror/mode/"
 var modes = [base + "clike/clike.js",
-				base + "coffeescript/coffeescript.js",
-				base + "css/css.js",
-				base + "diff/diff.js",
-				base + "gfm/gfm.js",
-				base + "go/go.js",
-				base + "htmlembedded/htmlembedded.js",				
-				base + "htmlmixed/htmlmixed.js",
-				base + "javascript/javascript.js",
-				base + "lua/lua.js",
-				base + "markdown/markdown.js",
-				base + "php/php.js",
-				base + "python/python.js",
-				base + "r/r.js",
-				base + "ruby/ruby.js",
-				base + "scheme/scheme.js",
-				base + "shell/shell.js",
-				base + "smalltalk/smalltalk.js",
-				base + "xml/xml.js"];
+            base + "coffeescript/coffeescript.js",
+            base + "css/css.js",
+            base + "diff/diff.js",
+            base + "gfm/gfm.js",
+            base + "go/go.js",
+            base + "htmlembedded/htmlembedded.js",				
+            base + "htmlmixed/htmlmixed.js",
+            base + "javascript/javascript.js",
+            base + "lua/lua.js",
+            base + "markdown/markdown.js",
+            base + "php/php.js",
+            base + "python/python.js",
+            base + "r/r.js",
+            base + "ruby/ruby.js",
+            base + "scheme/scheme.js",
+            base + "shell/shell.js",
+            base + "smalltalk/smalltalk.js",
+            base + "xml/xml.js"];
 
 LazyLoad.js(modes);
 
@@ -73,7 +73,6 @@ LazyLoad.js("js/application.js");
 
 LazyLoad.css("vendor/bootstrap/css/bootstrap.min.css");
 LazyLoad.css("vendor/bootstrap/css/bootstrap-responsive.min.css");
-
 
 LazyLoad.css("vendor/rainbow/themes/github.css");
 
